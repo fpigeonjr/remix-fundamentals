ac5af74f9de07e42250b1bf059ea08bba37bd86b
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2022-10-04 11:01:21 -0500

    adds markdown to posts

--- exercise/03-dynamic-params/app/routes/posts/$slug.tsx
+++ exercise/03-dynamic-params/app/routes/posts/$slug.tsx
@@ -1,20 +1,28 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { marked } from "marked";
 import { getPost } from "~/models/post.server";
 
 export async function loader({ params }: LoaderArgs) {
+  if (!params.slug) {
+    throw new Error("Missing slug");
+  }
   const post = await getPost(params.slug);
-  return json({ post });
+
+  if (!post) {
+    throw new Error("Post not found");
+  }
+  const html = marked(post.markdown);
+  return json({ title: post.title, html });
 }
 
 export default function PostRoute() {
-  const data = useLoaderData<typeof loader>();
+  const { title, html } = useLoaderData<typeof loader>();
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">
-        {data.post.title}
-      </h1>
+      <h1 className="my-6 border-b-2 text-center text-3xl">{title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
 }
